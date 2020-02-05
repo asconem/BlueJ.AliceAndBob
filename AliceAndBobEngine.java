c3ef95fb4863496c3b657c9f8762fe54911326d0
Matthew Ascone <mascone@zipcodes-MacBook-Pro-9.local>
2020-02-05 14:41:46 -0500

    Conditional logic added, all tests passed.

--- AliceAndBobEngine.java
+++ AliceAndBobEngine.java
@@ -1,14 +1,14 @@
 
 /**
- * Write a description of class AliceAndBobEngine here.
+ * This program is designed to evaluate the input string and output a different greeting accordingly
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Matthew Ascone)
+ * @version (2/5/20)
  */
 public class AliceAndBobEngine
 {
     String result;
-    
+
     AliceAndBobEngine(){
         result = "";
     }
@@ -16,7 +16,13 @@ public class AliceAndBobEngine
     //HINT: LOOK AT THE TESTS TO SEE WHAT IS EXPECTED FOR DIFFERENT CONDITIONS
     public String evaluateAliceOrBob(String input)
     {
-    
+    if (input.equals("Bob")) {
+      result = "Hello Bob";
+    } else if (input.equals("Alice")) {
+      result = "Hello Alice";
+    } else {
+      result = "I DON'T KNOW YOU, YOU ARE SCARY";
+    }
         return result;
     }
 }
